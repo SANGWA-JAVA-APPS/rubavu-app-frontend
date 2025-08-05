e44e9d398c5fff2c105649bdeac0a9d83eabd565
sangwa <sangwa22@gmail.com>
2025-08-05 08:12:54 +0200

    changed the live path and rebuilt

--- src/components/services/StockServices/StockConn.jsx
+++ src/components/services/StockServices/StockConn.jsx
@@ -13,8 +13,8 @@ export default class StockConn {
     };
     static port = {
         /*the below hosted to Djuma Nyabugogo and djuma town, the both have the same port bcz they are on different servers*/
-        // val: '8101/'  // this is online
-        val: '8097/'  // this is online
+        val: '8101/'  // this is offline
+        // val: '8097/'  // this is online
     }
     static basicPath = {
         val: 'codeguru/api',
@@ -27,7 +27,7 @@ export default class StockConn {
     static sbp = { /* Stock basic path (sbp) This is used on the settings, but it can/shall be used even for other endpoints, it maded in order to expand endpoinds*/
         name: StockConn.server.name + StockConn.port.val + StockConn.basicPath.stockVal
     }
-
+    
     static ReqContentType = 'application/json'
     static LoginToken = {
         'Content-Type': StockConn.ReqContentType,
